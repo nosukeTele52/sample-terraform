42d91da2469c1a4834918201dc7a73ca7efe148f
nosukeTele52 <1992monky@gmail.com>
2026-04-28 21:16:05 +0900

    local variable

---
+++ terraform/chapter4/README.md
@@ -0,0 +1,4 @@
+
+```
+terraform apply -var="filename=hello.txt" -var="content=Hello, Terraform Variables." -auto-approve
+```
--- terraform/chapter4/main.tf
+++ terraform/chapter4/main.tf
@@ -4,3 +4,17 @@ resource "local_file" "my_file" {
     filename    = var.filename
     content     = var.content
 }
+
+
+locals {
+    filename    = "local-ver-sample.txt"
+    content     = "hello local veriables"
+}
+
+# 文字列と組み合わせる場合は"${}"で囲う
+# example
+# filename = string_concat_${local.filename}.txt
+resource "local_file" "my_local_file" {
+    filename    = local.filename
+    content     = local.content
+}
